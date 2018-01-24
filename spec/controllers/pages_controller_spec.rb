04336633e13e49a8578e3f8d81808b8371be37d6
Kelly Munro <kelly.munro@fluxfederation.com>
2018-01-25 10:03:17 +1300

    Add pages controller and blank index template

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+  describe '#index' do
+    it 'returns http status 200' do
+      get :index
+
+      expect(response.status).to eq 200
+    end
+
+    it 'renders the index template' do
+      get :index
+
+      expect(response).to render_template :index
+    end
+  end
+end
