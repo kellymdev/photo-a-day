feeb69529072ba7808598ebcb2ebc3a78a1b67d5
Kelly Munro <kelly.munro@fluxfederation.com>
2018-01-26 10:16:48 +1300

    Add Create action for Photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,20 @@ class PhotosController < ApplicationController
     @subjects = Subject.all.order(:name)
     @photo = Photo.new
   end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:subject_id, :date, :image_url, :notes)
+  end
 end
--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -1,9 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe PhotosController, type: :controller do
+  let(:category) { Category.create!(name: 'Macro') }
+  let(:subject) { category.subjects.create!(name: 'Ant') }
+
   describe '#new' do
     it 'returns http status 200' do
       expect(response.status).to eq 200
+    end
+  end
+
+  describe '#create' do
+    let(:subject_id) { subject.id }
+    let(:date) { Date.today }
+    let(:photo_params) { { photo: { subject_id: subject_id, date: date, image_url: 'http://www.test.com/test.jpg', notes: 'Test' } } }
+
+    context 'with valid params' do
+      it 'creates a photo' do
+        expect { post :create, params: photo_params }.to change { Photo.count }.by 1
+      end
+    end
+
+    context 'with invalid params' do
+      context 'without a subject' do
+        let(:subject_id) { }
+
+        it 'does not create a photo' do
+          expect { post :create, params: photo_params }.to change { Photo.count }.by 0
+        end
+      end
+
+      context 'without a date' do
+        let(:date) { }
+
+        it 'does not create a photo' do
+          expect { post :create, params: photo_params }.to change { Photo.count }.by 0
+        end
+      end
     end
   end
 end
