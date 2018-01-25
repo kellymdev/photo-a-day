8af117ab33e5892fc37a2d76ebad0f87efd1b728
Kelly Munro <kelly.munro@fluxfederation.com>
2018-01-25 14:24:43 +1300

    Initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+landscape = Category.create!(name: 'Landscape')
+landscape.subjects.create!(name: 'Plimmerton Beach')
+
+macro = Category.create!(name: 'Macro')
+macro.subjects.create!(name: 'Ant')
+macro.subjects.create!(name: 'Bee')
+
+nature = Category.create!(name: 'Nature')
+nature.subjects.create!(name: 'Black-backed Gull')
+nature.subjects.create!(name: 'Tui')
+
+people = Category.create!(name: 'People')
+people.subjects.create!(name: 'Mother')
+
+pets = Category.create!(name: 'Pets')
+pets.subjects.create!(name: 'Cat')
+
+urban = Category.create!(name: 'Urban')
+urban.subjects.create!(name: 'Te Papa')
